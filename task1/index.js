82e3c3a889b4e7b2228010d1884c488b781d8d2d
Nazar Marusyn <marusinnazar@gmail.com>
2022-05-04 21:48:24 +0300

    Add dots calculator

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,23 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(str) {
+  let arr = str.split(' ');
+  let count;
+  
+  switch (arr[1]){
+    case '+':
+      return arr[0] + arr[2]
+    case '-':
+      count = arr[0].length - arr[2].length;
+      break;
+    case '*':
+      count = arr[0].length * arr[2].length;
+      break;
+    case "//":
+      count = Math.floor(arr[0].length / arr[2].length);
+      break;
+  }
+  return ".".repeat(count);
 }
+
+let res = calculate(".. // ...");
+
+console.log(res);
