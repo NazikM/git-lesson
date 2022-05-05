845d9536c0aa48ebc603ecb74070da2484201e15
Nazar Marusyn <marusinnazar@gmail.com>
2022-05-05 11:48:52 +0300

    task 2 done

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,20 @@
-function grid() {
-  //Here is your solution
-  return
+function grid(number) {
+  if(number < 0) return null;
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  let grid = [];
+
+  for(let i = 0; i < number; i++){
+    grid[i] = [];
+    for(let j = 0; j < number; j++){
+      grid[i][j] = alphabet[(j+i)%26];
+    }
+  }
+
+  return grid;
 }
+
+let res = grid(30);
+
+console.log(res)
