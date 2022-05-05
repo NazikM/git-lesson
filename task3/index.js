85902abfb90f43adfaec32f151fae2278a06b19c
Nazar Marusyn <marusinnazar@gmail.com>
2022-05-05 12:02:36 +0300

    task3 done

--- task3/index.js
+++ task3/index.js
@@ -1,3 +1,15 @@
-function isValidParentheses() {
-  //code
+function isValidParentheses(str) {
+  let stack = 0;
+  for(let i = 0; i < str.length; i++){
+    if(str[i] == "(") stack++;
+    else if(stack > 0){
+      stack--;
+    }else{
+      return false;
+    }
+  }
+  return stack > 0 ? false : true;
 }
+
+let res = isValidParentheses("(())((()())())")
+console.log(res);
